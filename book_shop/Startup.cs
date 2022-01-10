b40c4e422e429f492f96222fd4cf62c59e9e6309
ThuysNga <19521881@gm.uit.edu.vn>
2022-01-10 10:16:58 +0700

    create admin area

--- book_shop/Startup.cs
+++ book_shop/Startup.cs
@@ -49,8 +49,13 @@ namespace book_shop
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
